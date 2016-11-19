161256a30ace3366781739f257c7e9600e73e86a
Roman Dimov <romanddimov@gmail.com>
2016-11-19 00:44:26 -0800

    Trying Git, changes indentation, adds test comment

--- src/org/usfirst/frc/team5700/robot/Robot.java
+++ src/org/usfirst/frc/team5700/robot/Robot.java
@@ -1,7 +1,6 @@
 
 package org.usfirst.frc.team5700.robot;
 
-import org.usfirst.frc.team5700.robot.commands.LowBarAuto;
 import org.usfirst.frc.team5700.robot.subsystems.DefenseArms;
 import org.usfirst.frc.team5700.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team5700.robot.subsystems.Intake;
@@ -12,7 +11,6 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -23,50 +21,52 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot {
 	
+	//Git comment
+
 	public static DefenseArms defenseArms;
 	public static DriveTrain drivetrain;
 	public static Scaling scaling;
 	public static Intake intake;
 	public static OI oi;
-	
 
-    Command autonomousCommand;
-    SendableChooser chooser;
 
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
-        
-    	//smartDashboard = new SmartDashboard();
-    	
-        chooser = new SendableChooser();
-        //chooser.addObject("Low Bar Auto", new LowBarAuto());
-        
-        //chooser.addDefault("Default Auto", new ExampleCommand());
-		//chooser.addObject("My Auto", new MyAutoCommand());
-        //SmartDashboard.putData("Auto mode", chooser);
-    	//Scheduler.getInstance().add(TankDriveWithJoysticks);
-        
+	Command autonomousCommand;
+	SendableChooser chooser;
 
-        defenseArms = new DefenseArms();
-        drivetrain = new DriveTrain();
-        scaling = new Scaling();
-        intake = new Intake();
-        oi = new OI();
-        
-    }
-	
 	/**
-     * This function is called once each time the robot enters Disabled mode.
-     * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-     */
-    public void disabledInit(){
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	public void robotInit() {
 
-    }
-	
+		//smartDashboard = new SmartDashboard();
+
+		chooser = new SendableChooser();
+		//chooser.addObject("Low Bar Auto", new LowBarAuto());
+
+		//chooser.addDefault("Default Auto", new ExampleCommand());
+		//chooser.addObject("My Auto", new MyAutoCommand());
+		//SmartDashboard.putData("Auto mode", chooser);
+		//Scheduler.getInstance().add(TankDriveWithJoysticks);
+
+
+		defenseArms = new DefenseArms();
+		drivetrain = new DriveTrain();
+		scaling = new Scaling();
+		intake = new Intake();
+		oi = new OI();
+
+	}
+
+	/**
+	 * This function is called once each time the robot enters Disabled mode.
+	 * You can use it to reset any subsystem information you want to clear when
+	 * the robot is disabled.
+	 */
+	public void disabledInit(){
+
+	}
+
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
 	}
@@ -80,9 +80,9 @@ public class Robot extends IterativeRobot {
 	 * You can add additional auto modes by adding additional commands to the chooser code above (like the commented example)
 	 * or additional comparisons to the switch structure below with additional strings & commands.
 	 */
-    public void autonomousInit() {
-        autonomousCommand = (Command) chooser.getSelected();
-        
+	public void autonomousInit() {
+		autonomousCommand = (Command) chooser.getSelected();
+
 		/* String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
 		switch(autoSelected) {
 		case "My Auto":
@@ -93,37 +93,37 @@ public class Robot extends IterativeRobot {
 			autonomousCommand = new ExampleCommand();
 			break;
 		} */
-    	
-    	// schedule the autonomous command (example)
-        if (autonomousCommand != null) autonomousCommand.start();
-    }
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
+		// schedule the autonomous command (example)
+		if (autonomousCommand != null) autonomousCommand.start();
+	}
 
-    public void teleopInit() {
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	public void teleopInit() {
 		// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null) autonomousCommand.cancel();
-    }
+		// teleop starts running. If you want the autonomous to 
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (autonomousCommand != null) autonomousCommand.cancel();
+	}
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
 }
